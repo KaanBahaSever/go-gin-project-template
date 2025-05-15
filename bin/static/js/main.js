ccca5fec361ca2ccf6891a8cdee59d0cb80fa031
Kaan Baha Sever <kaanbahasever@gmail.com>
2025-05-15 23:42:53 +0300

    Delete bin directory

--- bin/static/js/main.js
+++
@@ -1,7 +0,0 @@
-// Main JavaScript file for the application
-console.log('Application loaded');
-
-// Add event listeners when DOM is ready
-document.addEventListener('DOMContentLoaded', function() {
-    // You can add more JavaScript functionality here
-});
